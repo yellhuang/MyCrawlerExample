af3196b9499b241f5f2bc35730a49e84103b8443
yellhuang <yellhuang63@gmail.com>
2017-04-08 20:20:51 +0800

    可更新測試

--- src/crawler/example/BasicExample.java
+++ src/crawler/example/BasicExample.java
@@ -10,7 +10,7 @@ import org.apache.commons.logging.impl.SimpleLog;
  *
  */
 public class BasicExample {
-	// commit test (yell )
+	// commit test (yell test)
 	public static void main(String[] args) {
 
 		// set to debug level
