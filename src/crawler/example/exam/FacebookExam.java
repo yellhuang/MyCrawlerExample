66311ecdc6ef5859eac6bff646cd88473577aa74
yellhuang <yellhuang63@gmail.com>
2017-04-09 19:22:00 +0800

    use app token, 抓取科技橘報粉絲團前10筆文章的id、按讚總數、心情總數(不包含讚)

--- src/crawler/example/exam/FacebookExam.java
+++ src/crawler/example/exam/FacebookExam.java
@@ -13,27 +13,27 @@ import org.jsoup.select.Elements;
  * 1. 先利用Graph Api調整出需要的資料
  * 2. 修改程式，使用爬蟲包取得資料
  * 3. 上傳至GitHub
- * 
+ *
  * @author Abola Lee
  *
  */
 public class FacebookExam {
-	
+
 	public static void main(String[] args) {
-		
+
 		// 遠端資料路徑
 
-		String uri = 
+		String uri =
 				"https://graph.facebook.com/v2.8"
-				+ "/TechOrange/feed?limit=10&fields=id,message,likes.limit(0).summary(total_count),reactions.limit(0).summary(total_count)"
-				+ "&access_token=EAABr2StPNWABAJw70G5NHGvrfHSIAOMqvvZCKtzwsXfY8S4tVlheLbTsp1OzI3cfENzCxjbjxrkw4NknlZA8G1rliIv7GfZBYHZBNh3QZBPa0tKMKHhePc6gIS6xd39ieB3bZBeZB3t8sGcZCRJl5gLRvkbfgHlOZAZAHRJC8ZBVvaZB8kWp0iv3CwEeXmnEuDu07n8ZD";
+						+ "/TechOrange/feed?limit=10&fields=id,message,likes.limit(0).summary(total_count),reactions.limit(0).summary(total_count)"
+						+ "&access_token=118580478686560%7COf5Y7jVzJx_qbhiLKWc1v7qD9cM";
 
 
 		Elements elems =
 				CrawlerPack.start()
-				.getFromJson(uri)
-				.select("data");
-		
+						.getFromJson(uri)
+						.select("data");
+
 		String output = "id,讚總數,心情總數" + "\n";
 
 		// 遂筆處理
@@ -48,5 +48,5 @@ public class FacebookExam {
 		}
 
 		System.out.println( output );
-	} 
+	}
 }
